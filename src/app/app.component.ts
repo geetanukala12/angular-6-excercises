40080bdd65f51ce088588f737545ec66c1463c96
GNukala <GNukala@rsna.org>
2018-10-03 16:45:16 +0530

    basic login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl  } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sampleApp';
+  title = 'demo';
+  submitted = false;
+
+  loginForm = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl()
+  });
+  onSubmit() {
+      console.log(this.loginForm)
+      this.submitted = true;
+      console.log("email is"+this.loginForm.controls.email.value)
+      console.log("password is"+this.loginForm.controls.password.value)
+  }
+
+  login(email:string, password:string){
+    
+  }
 }
